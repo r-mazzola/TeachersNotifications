668a5d150210a6325ae484d6393409b9b3427edd
ArriKing <arrigonimarco91@gmail.com>
2017-06-09 11:23:19 +0200

    Aggiuunta Spinner corsi + gestione messaggi

--- app/src/main/java/com/project/rm/teachersnotifications/MainActivity.java
+++ app/src/main/java/com/project/rm/teachersnotifications/MainActivity.java
@@ -3,8 +3,10 @@ package com.project.rm.teachersnotifications;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -17,9 +19,12 @@ public class MainActivity extends AppCompatActivity {
     TextView tvMsg;
     EditText etMsg;
     Button btSend;
+    private String cCorso;
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mConditionRef = mRootRef.child("condition");
+
+    //DatabaseReference mMessRef = mAutomaticaRef.child("Messaggio");
+    //DatabaseReference mSisopRef = mRootRef.child("Sistemi Operativi");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +36,44 @@ public class MainActivity extends AppCompatActivity {
         btSend =(Button)findViewById(R.id.btSend);
     }
 
+    private void Corso_selected(String t){cCorso=t;}
+
     @Override
     protected void onStart() {
         super.onStart();
-        mConditionRef.addValueEventListener(new ValueEventListener() {
+
+        //SPINNER SELEZIONE CORSI
+        Spinner sp=(Spinner) findViewById(R.id.spCorso);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> adapter, View view, int pos, long id) {
+                String selected = (String)adapter.getItemAtPosition(pos);
+                Corso_selected(selected);
+            }
+            public void onNothingSelected(AdapterView<?> arg0) {}
+        });
+
+
+        /*mMessRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String text = dataSnapshot.getValue(String.class);
                 tvMsg.setText(text);
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });
+        });*/
 
         btSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String msg = etMsg.getText().toString();
-                mConditionRef.setValue(msg);
+                DatabaseReference mCorsoRef = mRootRef.child(cCorso);
+                DatabaseReference mMessRef = mCorsoRef.child("Messaggio");
+                DatabaseReference mNomeCorsoRef = mRootRef.child("NomeCorso");
+                mNomeCorsoRef.setValue(cCorso);
+                mMessRef.setValue(msg);
             }
         });
 
